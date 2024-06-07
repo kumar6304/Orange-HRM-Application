6da14264a3f504f87c3feaf0cb3b9c1f55ce3f78
kumar6304 <bargav08222@gmail.com>
2024-06-07 19:58:23 +0530

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,22 @@
+package com.Basicjava;
+
+public class MyFirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This my first java program");
+		
+		System.out.print("This my first selenium code");
+		System.out.println("This my first testcase");
+		System.out.print("This my first manual class");
+		System.out.print("This my first selenium class");
+		
+		//This program is written to print a meaningful message as an output on the system
+		//+ - is called as concatenation symbol
+		
+		System.out.println("This is not a java program-it's a statements of ravi");
+		System.out.println("To learn selenium webdriver"+"To Test web Application");
+
+	}
+
+}
